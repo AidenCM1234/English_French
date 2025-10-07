ec05d8f0db376c6d7140a03ba45b2cee7a7a2e3d
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-07 09:26:27 -0400

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,3 +1,8 @@
-
-import random
-random_int = random.randint(1, 10)
+def spaces(y,t):
+    x=0
+    
+    for i in y:
+        if x[i] == y and i == t:
+            x+=1
+    
+spaces("ccc..",".c.c.c")
