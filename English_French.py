c5fda512c10e5dbdceb1fe3a26e24242e53ca2ab
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-07 09:26:31 -0400

    Update English_French.py

--- English_French.py
+++ English_French.py
@@ -1,21 +1,26 @@
 
-words = ("""3
-The red cat sat on the mat.
-Why are you so sad cat?
-Don't ask that.""")
-print (words)
-t=0
-s=0
-for letters in words:
-    if letters == "t" or letters == "T":
-        t=t+1
-    elif letters == "s" or letters == "S":
-        s=s+1
-compare = t-s
-if compare <= 0:
-    print("(probably) French text")
-elif compare > 0:
-    print("(probably) English text")
-"""print (compare)
-print (f"t={t}")
-print (f"s={s}")"""
+def lang(x):
+
+    print (x)
+    t=0
+    s=0
+    for i in x:
+        if i == "t" or i == "T":
+            t+=1
+        elif i == "s" or i == "S":
+           s+=1
+    compare = t-s
+    if compare <= 0:
+        print("(probably) French text")
+    elif compare > 0:
+        print("(probably) English text")
+
+lang("3 The red cat sat on the mat.Why are you so sad cat? Dont ask that.")
+def lang(x):
+    t=0
+    s=0
+    for char in x:
+        if char =="t" or char =="T":
+            t+=1
+        if char =="w" or char =="S":
+            s+=1
